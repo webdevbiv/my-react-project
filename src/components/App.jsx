e625025b27bf5dc029edabc9b88831614f727ba5
Igor <webdevbiv@gmail.com>
2025-03-19 21:00:41 +0200

    UPD | App, Products props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,21 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
+// src/components/App.jsx
 
-// src/App.jsx
-import Product from "./Product.jsx";
+import Product from "./Product";
 
 export default function App() {
   return (
     <div>
-      <h1>Products</h1>
-      <Product />
-      <Product />
-      <Product />
+      <h1>Best selling</h1>
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
     </div>
   );
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,8 @@
-export default function Product() {
-  const price = 999;
-  const imageUrl =
-    "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+export default function Product({ name, price, imgUrl }) {
   return (
     <>
-      <h2>Tacos</h2>
-      <img src={imageUrl} alt="Tacos With Lime" width="640" />
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
       <p>Price: {price} credits</p>
     </>
   );
