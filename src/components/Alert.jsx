be47afa847b88009935e5de2df861ab903b92c32
Igor <webdevbiv@gmail.com>
2025-03-20 00:59:06 +0200

    UPD | Alert component

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,35 +1,53 @@
-const alertStyles = {
-  margin: 8,
-  padding: "12px 16px",
-  borderRadius: 4,
-  backgroundColor: "gray",
-  color: "white",
-};
+// import styles from "./Alert.css";
 
-const getBgColor = (variant) => {
-  switch (variant) {
-    case "info":
-      return "blue";
-    case "success":
-      return "green";
-    case "error":
-      return "red";
-    case "warning":
-      return "orange";
-    default:
-      throw new Error(`Unsupported variant prop value - ${variant}`);
+// const alertStyles = {
+//   margin: 8,
+//   padding: "12px 16px",
+//   borderRadius: 4,
+//   backgroundColor: "gray",
+//   color: "white",
+// };
+
+// const getBgColor = (variant) => {
+//   switch (variant) {
+//     case "info":
+//       return "blue";
+//     case "success":
+//       return "green";
+//     case "error":
+//       return "red";
+//     case "warning":
+//       return "orange";
+//     default:
+//       throw new Error(`Unsupported variant prop value - ${variant}`);
+//   }
+// };
+
+// export const Alert = ({ variant, children }) => {
+//   return (
+//     <p
+//       style={{
+//         ...styles.alert,
+//         backgroundColor: getBgColor(variant),
+//       }}
+//     >
+//       {children}
+//     </p>
+//   );
+// };
+
+import "./Alert.css";
+
+export const Alert = ({ variant, outlined, elevated, children }) => {
+  const classNames = ["alert", variant];
+
+  if (outlined) {
+    classNames.push("is-outlined");
   }
-};
 
-export const Alert = ({ variant, children }) => {
-  return (
-    <p
-      style={{
-        ...alertStyles,
-        backgroundColor: getBgColor(variant),
-      }}
-    >
-      {children}
-    </p>
-  );
+  if (elevated) {
+    classNames.push("is-elevated");
+  }
+
+  return <p className={classNames.join(" ")}>{children}</p>;
 };
